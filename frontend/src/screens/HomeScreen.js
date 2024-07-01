6a9b89ac64908f89de0244aaf9664e07507d4073
CodeWithNomanone <noman.one@outlook.com>
2024-07-01 18:21:31 +0400

    Added pagination to SearchScreen and update css

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 import Product from '../components/Product';
@@ -14,6 +14,14 @@ export default function HomeScreen() {
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 4;
+  const totalPages = Math.ceil(products.length / productsPerPage);
+  const currentProducts = products.slice(
+    (currentPage - 1) * productsPerPage,
+    currentPage * productsPerPage
+  );
+
   const userTopSellersList = useSelector((state) => state.userTopSellersList);
   const {
     loading: loadingSellers,
@@ -25,6 +33,9 @@ export default function HomeScreen() {
     dispatch(listProducts({}));
     dispatch(listTopSellers());
   }, [dispatch]);
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
   return (
     <div>
       <h1 className="h1white">Top Sellers</h1>
@@ -60,9 +71,32 @@ export default function HomeScreen() {
         <>
           {products.length === 0 && <MessageBox>No Product Found</MessageBox>}
           <div className="row center">
-            {products.map((product) => (
+            {currentProducts.map((product) => (
               <Product key={product._id} product={product}></Product>
             ))}
+          </div>
+          <div className="pagination">
+            <button
+              disabled={currentPage === 1}
+              onClick={() => handlePageChange(currentPage - 1)}
+            >
+              Previous
+            </button>
+            {[...Array(totalPages).keys()].map((x) => (
+              <button
+                key={x + 1}
+                className={currentPage === x + 1 ? 'active' : ''}
+                onClick={() => handlePageChange(x + 1)}
+              >
+                {x + 1}
+              </button>
+            ))}
+            <button
+              disabled={currentPage === totalPages}
+              onClick={() => handlePageChange(currentPage + 1)}
+            >
+              Next
+            </button>
           </div>
         </>
       )}
